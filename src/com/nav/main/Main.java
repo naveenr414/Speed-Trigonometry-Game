eaeba2d9fdcffa061c21b5207ceb21b2193f65ce
Naveen Raman <dsfan414@gmail.com>
2014-11-03 19:04:18 -0500

    Rendered Latex
    The latex code is now able to be rendered.

--- src/com/nav/main/Main.java
+++ src/com/nav/main/Main.java
@@ -1,7 +1,17 @@
 package com.nav.main;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.image.BufferedImage;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import org.scilab.forge.jlatexmath.TeXConstants;
+import org.scilab.forge.jlatexmath.TeXFormula;
+import org.scilab.forge.jlatexmath.TeXIcon;
 
 import com.nav.latex.ProblemToLatex;
 import com.nav.trig.ProblemGenerator;
@@ -33,9 +43,35 @@ public class Main
 		panel = new JPanel();
 		frame.add(panel);
 		
-		frame.setVisible(true);
+		String latex = ProblemToLatex.problemToLatex(ProblemGenerator.genProblem(true, true, false, false));
 		
-		//Testing
-		System.out.println(ProblemToLatex.problemToLatex(ProblemGenerator.genProblem(true, true, false, false)));
+		
+		frame.setVisible(true);
+		genLatex(latex);
+		frame.setVisible(true);
+	}
+	
+	public static void genLatex(String latex)
+	{
+		TeXFormula formula = new TeXFormula(latex);
+		
+		// render the formla to an icon of the same size as the formula.
+		TeXIcon icon = formula
+				.createTeXIcon(TeXConstants.STYLE_DISPLAY, 20);
+		
+		// insert a border 
+		icon.setInsets(new Insets(5, 5, 5, 5));
+
+		// now create an actual image of the rendered equation
+		BufferedImage image = new BufferedImage(icon.getIconWidth(),
+				icon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g2 = image.createGraphics();
+		g2.setColor(Color.white);
+		g2.fillRect(0, 0, icon.getIconWidth(), icon.getIconHeight());
+		JLabel jl = new JLabel();
+		jl.setForeground(new Color(0, 0, 0));
+		icon.paintIcon(jl, g2, 0, 0);
+		
+		panel.getGraphics().drawImage(image, 0, 0, null);
 	}
 }
---
+++ src/com/nav/main/MainFrame.java
@@ -0,0 +1,19 @@
+package com.nav.main;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame
+{
+	public MainFrame(int width,int height)
+	{
+		setSize(width,height);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		setLocationRelativeTo(null);
+	}
+	
+	public void render()
+	{
+		
+	}
+}
